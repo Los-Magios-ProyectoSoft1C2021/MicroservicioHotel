b57a5986937a29b3e57bfda241cda52fe323338f
matibf99 <matibf99@gmail.com>
2021-04-26 01:25:46 -0300

    Añadidas clases de configuración para todas las entidades.

--- Template.AccessData/Configuration/FotoHotelConfiguration.cs
+++ Template.AccessData/Configuration/FotoHotelConfiguration.cs
@@ -11,7 +11,21 @@ namespace MicroservicioHotel.AccessData.Configuration
     {
         public void Configure(EntityTypeBuilder<FotoHotel> builder)
         {
-            
+            builder.HasKey(fh => fh.FotoHotelId);
+            builder.Property(fh => fh.FotoHotelId)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(fh => fh.ImagenUrl)
+                .IsRequired(true)
+                .HasMaxLength(128);
+
+            builder.Property(fh => fh.Descripcion)
+                .IsRequired(true)
+                .HasMaxLength(128);
+
+            builder.HasOne(fh => fh.Hotel)
+                .WithMany(h => h.FotosHotel)
+                .HasForeignKey(fh => fh.HotelId);
         }
     }
 }
---
+++ Template.AccessData/HotelDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using MicroservicioHotel.Domain.Entities;
+using MicroservicioHotel.AccessData.Configuration;
+
+namespace MicroservicioHotel.AccessData
+{
+    public class HotelDbContext : DbContext
+    {
+        public DbSet<Hotel> Hoteles { get; set; }
+        public DbSet<Habitacion> Habitaciones { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<FotoHotel> FotosHoteles { get; set; }
+
+        public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Hotel>(new HotelConfiguration());
+            modelBuilder.ApplyConfiguration<FotoHotel>(new FotoHotelConfiguration());
+            modelBuilder.ApplyConfiguration<Categoria>(new CategoriaConfiguration());
+            modelBuilder.ApplyConfiguration<Habitacion>(new HabitacionConfiguration());
+        }
+    }
+}
